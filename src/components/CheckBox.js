c482b1889956a9ca582755f296776ba32ad635c5
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2021-03-21 00:00:00 +1000

    form validation

---
+++ src/components/CheckBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useField } from 'formik';
+
+const CheckBox = ({ ...props }) => {
+
+  const [field, meta] = useField({ ...props, type: "checkbox" });
+  return (
+    <>
+      <label className="form__field">
+        <input className="form__input form__input_checkbox" {...field} {...props} type="checkbox" />
+        <span className="form__checkbox-text">
+          Согласен с <a className="form__checkbox-accent" target="_blank" href='#'>офертой</a>
+        </span>
+      </label>
+      {meta.touched && meta.error && <span className="form__item-error">{meta.error}</span>}
+    </>
+  );
+};
+
+export default CheckBox;
